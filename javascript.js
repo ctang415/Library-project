bcf4d29fec6d0034b036a91fb0883039ec9e4965
ctang415 <carrietang117@gmail.com>
2022-09-18 15:07:32 -0400

    Set background to darken when form pops up

--- javascript.js
+++ javascript.js
@@ -7,18 +7,16 @@ const add = document.getElementById('add');
 const form = document.getElementById('form');
 const close = document.getElementById('close')
 
-function closeForm() {
-    document.getElementById('form').style.display = "none";
-}
-
-closeForm();
-
 add.addEventListener('click', function() {
     document.getElementById('form').style.display = "block";
+    document.getElementById('loginform').style.display = 'block';
+
 });
 
 close.addEventListener('click', function(){
     document.getElementById('form').style.display = "none";
+    document.getElementById('loginform').style.display = 'none';
+
 })
 
 
@@ -30,7 +28,7 @@ function Book(title, author, pages, read) {
     this.pages = pages
     this.read = read
     this.info = function() {
-        return this.title + ' by ' + this.author + ", " + this.pages + 'pages, ' + this.read
+        return this.title + ' by ' + this.author + ", " + this.pages + ' pages, ' + this.read
     }
 }
 
@@ -47,19 +45,21 @@ function Book(title, author, pages, read) {
     clearValue();
     }
 });
+*/
 
-
-/*button.addEventListener('click', function() {
+button.addEventListener('click', function() {
     if (bookTitle.value === '' || bookAuthor.value === '' || bookPages.value === '' || bookRead.value === '') {
         !button;
     }
     else {
         const book = new Book(bookTitle.value, bookAuthor.value, bookPages.value, bookRead.value)
         clearValue();
+        document.getElementById('form').style.display = "none";
+        document.getElementById('loginform').style.display = 'none';
         console.log(book.info());
     }
 });
-*/
+
 
 function clearValue() {
     bookTitle.value = '';
