e17472c76d2f69bcc80ba348638b6d7e5e1d4441
ctang415 <carrietang117@gmail.com>
2022-09-19 14:36:30 -0400

    Created function to display books from array

--- javascript.js
+++ javascript.js
@@ -23,27 +23,13 @@ let myLibrary = [];
 function Book(title, author, pages, read) {
     this.title = title
     this.author = author
-    this.pages = pages
+    this.pages = pages + " pages"
     this.read = read
     this.info = function() {
         return this.title + ' by ' + this.author + ", " + this.pages + ' pages, ' + this.read
     }
 }
 
-
-/*button.addEventListener('click', function() {
-    if (bookTitle.value === '' || bookAuthor.value === '' || bookPages.value === '' || bookRead.value === '') {
-        !button;
-    }
-    else {
-    myLibrary.push(bookTitle.value);
-    myLibrary.push(bookAuthor.value);
-    myLibrary.push(bookPages.value);
-    myLibrary.push(bookRead.value);
-    clearValue();
-    }
-});
-*/
 
 let book;
 
@@ -59,19 +45,54 @@ button.addEventListener('click', function() {
         document.getElementById('loginform').style.display = 'none';
         console.log(book.info());
         myLibrary.push(book);
+        addBookToLibrary();
     }
 });
 
-/*
-function clearValue() {
-    bookTitle.value = '';
-    bookAuthor.value = '';
-    bookPages.value = '';
-    bookRead.value = '';
-}
-*/
-
+const container = document.querySelector('.container');
 
 function addBookToLibrary() {
-    
+    const newDiv = document.createElement('div');
+    newDiv.setAttribute('style', 'white-space: pre; background-color: #AD6A34;')
+    newDiv.textContent = book.title;
+    newDiv.textContent += "\r\n";
+    newDiv.textContent += book.author;
+    newDiv.textContent += "\r\n";
+    newDiv.textContent += book.pages;
+    if (book.read === 'unread') {
+        const newButton = document.createElement('button');
+        newDiv.textContent += "\r\n";
+        newButton.textContent = "Not read";
+        newDiv.appendChild(newButton)
+     }
+     else if (book.read === 'read') {
+        const newButton = document.createElement('button');
+        newDiv.textContent += "\r\n";
+        newButton.textContent = "Read";
+        newDiv.appendChild(newButton)
+     }
+     container.appendChild(newDiv);
 }
+
+
+/*
+
+            
+        }
+             book.title;
+             book.author;
+             book.pages;
+             if (book.read === 'unread') {
+                const newButton = document.createElement('button');
+                newButton.textContent = "Not read";
+                container.appendChild(newButton)
+                container.appendChild(newDiv);
+             }
+             else if (book.read === 'read') {
+                const newButton = document.createElement('button');
+                newButton.textContent = "Read";
+                container.appendChild(newButton)
+                container.appendChild(newDiv);
+             }
+        }
+*/
